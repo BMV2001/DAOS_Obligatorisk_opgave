c66ee8c21b3dff49df2f18035679f31340416c8b
Eren <Kader-2@hotmail.com>
2024-10-04 09:06:56 +0200

    GG

--- src/JDBC.java
+++ src/JDBC.java
@@ -1,2 +1,17 @@
+import java.sql.*;
+import java.util.ArrayList;
+
 public class JDBC {
+    private static Connection minConnection;
+
+    static {
+        try {
+            minConnection = DriverManager
+                    .getConnection("jdbc:sqlserver://localhost\\SQLExpress;databaseName=KarakterRegistrering;user=sa;password=Erenbaran19;");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
+
