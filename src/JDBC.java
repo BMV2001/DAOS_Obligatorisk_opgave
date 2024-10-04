f798641e2ec12aa100c5ecaf26a4878a28b816ca
2001b <eaa24bmv@students.eaaa.dk>
2024-10-04 09:29:29 +0200

    opgA testrun

--- src/JDBC.java
+++ src/JDBC.java
@@ -1,17 +1,43 @@
+import java.io.PrintWriter;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class JDBC {
     private static Connection minConnection;
+    private static Scanner scan = new Scanner(System.in);
 
     static {
         try {
             minConnection = DriverManager
-                    .getConnection("jdbc:sqlserver://localhost\\SQLExpress;databaseName=KarakterRegistrering;user=sa;password=Erenbaran19;");
+                    .getConnection("jdbc:sqlserver://localhost\\SQLExpress;databaseName=DAOS_Projekt;user=sa;password=010401;");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
+    public static void main(String[] args) throws SQLException {
+    opgA();
+
+    }
+
+    public static void opgA() throws SQLException {
+        //parameter for eksamensforsøg
+        String studID;
+        String afviklingsID;
+        System.out.println("Indtast venligst et studieID(tal ID) for en studerende, og en afvklings ID(tal ID) for den tilknyttede eksamen:");
+        studID = scan.next();
+        afviklingsID = scan.next();
+
+        String sqlStatement = "INSERT INTO Eksamensforsøg VALUES (?, ?, ?)";
+        PreparedStatement prestmt = minConnection.prepareStatement(sqlStatement);
+        prestmt.setInt(2, Integer.parseInt(studID));
+        prestmt.setInt(3, Integer.parseInt(afviklingsID));
+
+        prestmt.execute();
+
+        System.out.println("Studerende tilmeldt eksamensafvikling / Eksamensforsøg oprettet");
+
+    }
 }
 
