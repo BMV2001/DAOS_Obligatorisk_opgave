68a34aae6b2aa11f24141253e1442a3d06715fc4
2001b <eaa24bmv@students.eaaa.dk>
2024-10-04 09:39:10 +0200

    OPGA v.2,5

--- src/JDBC.java
+++ src/JDBC.java
@@ -10,7 +10,7 @@ public class JDBC {
     static {
         try {
             minConnection = DriverManager
-                    .getConnection("jdbc:sqlserver://localhost\\SQLExpress;databaseName=DAOS_Projekt;user=sa;password=010401;");
+                    .getConnection("jdbc:sqlserver://localhost\\SQLExpress;databaseName=DAOS_Projekt;user=sa;password=2001;");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -21,23 +21,35 @@ public class JDBC {
 
     }
 
-    public static void opgA() throws SQLException {
+    public static void opgA() {
         //parameter for eksamensforsøg
-        String studID;
-        String afviklingsID;
-        System.out.println("Indtast venligst et studieID(tal ID) for en studerende, og en afvklings ID(tal ID) for den tilknyttede eksamen:");
-        studID = scan.next();
-        afviklingsID = scan.next();
+        try {
+            String studID;
+            String afviklingsID;
+            String karakter;
+            System.out.println("Indtast studeID:");
+            studID = scan.next();
+            System.out.println("indtast afviklingsID for eksamen:");
+            afviklingsID = scan.next();
+            System.out.println("indtast gyldig karakter eller adminstrativ bedømmelse:");
+            karakter = scan.next();
 
-        String sqlStatement = "INSERT INTO Eksamensforsøg VALUES (?, ?, ?)";
-        PreparedStatement prestmt = minConnection.prepareStatement(sqlStatement);
-        prestmt.setInt(2, Integer.parseInt(studID));
-        prestmt.setInt(3, Integer.parseInt(afviklingsID));
 
-        prestmt.execute();
 
-        System.out.println("Studerende tilmeldt eksamensafvikling / Eksamensforsøg oprettet");
+            String sqlStatement = "INSERT INTO Eksamensforsøg VALUES (?, ?, ?)";
+            PreparedStatement prestmt = minConnection.prepareStatement(sqlStatement);
+            prestmt.setString(1, karakter);
+            prestmt.setInt(2, Integer.parseInt(studID));
+            prestmt.setInt(3, Integer.parseInt(afviklingsID));
 
+            prestmt.execute();
+
+            System.out.println("Studerende tilmeldt eksamensafvikling / Eksamensforsøg oprettet");
+        }
+        catch (SQLException e){
+            System.out.printf(e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 }
 
